0db0b8a68a38896a99efbf4253cd0c1bd4753a91
Daniel Zhao <daniel.zhao@weebly.com>
2019-07-14 09:46:54 -0700

    update logo and font

--- web/src/App.vue
+++ web/src/App.vue
@@ -6,14 +6,11 @@
 </template>
 
 <style>
-/*@import url(http://webfonts-plus.herokuapp.com?family=Graphik);*/
+@import url('https://fonts.googleapis.com/css?family=Oswald&display=swap');
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
 }
 </style>
 
