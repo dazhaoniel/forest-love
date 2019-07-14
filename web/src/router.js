4db9bd7d9131759bda2575fa22ce3090c7b65a2d
Daniel Zhao <daniel.zhao@weebly.com>
2019-07-13 23:22:42 -0700

    update page

---
+++ web/src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Hello from './components/Hello.vue';
+import About from './components/About.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+  ],
+});
