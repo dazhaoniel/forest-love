f94ebb01a84f303346b8ea960a6faa8e32849b0b
Daniel Zhao <daniel.zhao@weebly.com>
2019-07-14 01:41:24 -0700

    update path to logo

--- web/src/components/About.vue
+++ web/src/components/About.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h4>Travel inspires us, but we are also aware of what this means for the planet.</h4>
-    <h4>JFK <> </h4>
-    <h4>5845 kg</h4>
+    <h4>JFK > PEK</h4>
+    <h4>2923 kg</h4>
     <h4>Why is carbon offsetting important?</h4>
-    <h4>It balances out the carbon humans created through</h4>
-    <i/><p>reforestation</p>
-    <i/><p>sequestration</p>
-    <i/><p>investing in renewable energy</p>
+    <h4>It balances out the carbon we created through</h4>
+    <p>reforestation</p>
+    <p>sequestration</p>
+    <p>investing in renewable energy</p>
     <h4>Travel Better.</h4>
     <h2>Forest Love</h2>
     <h4>Our mission is simple.</h4>
