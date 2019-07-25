7668aa970213de9660c05c93bf7a6036bb335c7d
jshaw22 <jshaw22@gmail.com>
2019-07-24 23:12:30 -0700

    - Refactored search inputs to use react-select's AsyncSelect library to get select options - Debounce search to 400 milliseconds so server doesn't die - add bootstrap - hook up calculate totals functionality by hitting /distances endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,112 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import * as apiHelper from './api.helper';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as apiHelper from "./api.helper";
+import AsyncSelect from "react-select/async";
+import debounce from "debounce-promise";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      originCity: '',
-      destinationCity: '',
-      originCityList: []
+      originAirportCode: "",
+      destinationAirportCode: "",
+      roundTripChecked: false,
+      showResults: false,
+      totalDistance: '',
+      totalCarbon: ''
     };
-    this.updateOrigin = this.updateOrigin.bind(this);
-    this.updateDestination = this.updateDestination.bind(this);
-    this.searchCities = this.searchCities.bind(this);
-
-  }
-  componentDidMount () {
-
+    this.getAirportOptions = debounce(this.getAirportOptions.bind(this), 400);
+    this.selectOrigin = this.selectOrigin.bind(this);
+    this.selectDestination = this.selectDestination.bind(this);
+    this.changeChecked = this.changeChecked.bind(this);
+    this.getResults = this.getResults.bind(this);
   }
 
-  updateOrigin (e) {
-    this.setState({originCity: e.target.value});
-  }
+  componentDidMount() {}
 
-  updateDestination (e) {
-    this.setState({destinationCity: e.target.value});
-  }
-
-  searchCities(e) {
-    e.preventDefault();
-    if(e.keyCode === 13) {
-      const inputName = e.target.name;
-      if (inputName === "originInput") {
-        apiHelper.getAirports(this.state.originCity)
-        .then(airports => {
-          this.setState({
-            originCity: '',
-            originCityList: airports
-          });
-        });
-      }
-    }
-  }
-
-  renderOriginCities() {
-    if (this.state.originCityList.length > 0) {
-      return this.state.originCityList.map(city => {
-        return <li className="airportList">Airport code {city.iata} in {city.name}, {city.country}</li>
+  getAirportOptions(input, originOrDestination) {
+    if (!input) return;
+    return apiHelper.getAirports(input).then(airports => {
+      return airports.map(airport => {
+        const airportObj = {
+          value: airport.iata,
+          label: `${airport.iata} ${airport.name}`
+        };
+        return airportObj;
       });
-    }
+    });
+  }
+
+  selectOrigin(selected) {
+    console.log("selected", selected);
+    this.setState({ originAirportCode: selected.value });
+  }
+
+  selectDestination(selected) {
+    this.setState({ destinationAirportCode: selected.value });
+  }
+
+  changeChecked() {
+    this.setState({ roundTripChecked: !this.state.roundTripChecked });
+  }
+  
+  getResults() {
+    const {originAirportCode, destinationAirportCode, roundTripChecked} = this.state;
+    return apiHelper.getResults(originAirportCode, destinationAirportCode, roundTripChecked).then(result => {
+      this.setState({
+        totalCarbon: Math.round(result.totalCarbon),
+        totalDistance: Math.round(result.totalDistance),
+        showResults: true
+      });
+    })
+  }
+
+  renderResults() {
+    if (this.state.showResults)
+    return (
+      <div className="totals">
+        <div>You traveled a total distance of <strong>{this.state.totalDistance}</strong>km and emitted <strong>{this.state.totalCarbon}</strong>kg of carbon!
+        </div>
+      </div>
+    );
   }
 
   render() {
     return (
-      <div className="">
-        <input className="airportInputs" onKeyUp={this.searchCities} name="originInput" onChange={this.updateOrigin} type="text" value={this.state.originCity} placeholder="Origin city name or airport code"/> <br/>
-        {this.renderOriginCities()}
-        <input className="airportInputs" onChange={this.updateDestination} name="destinationInput" type="text" value={this.state.destinationCity} placeholder="Destination city name or airport code"/><br/>
-        <p>Round trip flight?</p>
-        <input type="checkbox"/>
+      <div className="forest-love-main">
+        <div className="searchContainer">
+          <AsyncSelect
+            getOptionLabel={this.getOptionLabel}
+            loadOptions={input => this.getAirportOptions(input, "origin")}
+            className="search-column"
+            placeholder="Search for origin city"
+            onChange={this.selectOrigin}
+          />
+          <AsyncSelect
+            getOptionLabel={this.getOptionLabel}
+            loadOptions={input => this.getAirportOptions(input, "destination")}
+            className="search-column"
+            placeholder="Search for destination city"
+            onChange={this.selectDestination}
+          />
+          <div className="custom-control custom-checkbox search-column">
+            <input
+              type="checkbox"
+              onChange={this.changeChecked}
+              checked={this.state.roundTripChecked}
+              className="custom-control-input"
+              id="roundTripCheck"
+            />
+            <label className="custom-control-label" htmlFor="roundTripCheck">
+              Round Trip?
+            </label>
+          </div>
+        </div>
+        <button type="button" onClick={this.getResults} className="searchButton btn btn-success">
+          See my carbon footprint!
+        </button>
+        {this.renderResults()}
       </div>
     );
   }
--- client/src/api.helper.js
+++ client/src/api.helper.js
@@ -10,3 +10,23 @@ export async function getAirports(name) {
     .catch(err => console.log(err));
     return response;
 }
+
+export async function getResults(origin, destination, isRoundTrip) {
+    // hard coding for now, multiple ones to come later
+    const response = await axios
+    .post('/api/v1/distances', {
+        'data': [{'start': origin, 'end': destination}]
+    })
+    .then(resp => {
+        const flightData = resp.data;
+        let { total_carbon, total_distance } = flightData; 
+        if (isRoundTrip) {
+            total_carbon = total_carbon * 2;
+            total_distance = total_distance * 2;
+        }
+        // because we use camelCase on the front end ;)
+        return {totalCarbon: total_carbon, totalDistance: total_distance};
+    })
+    .catch(err => console.log(err));
+    return response;
+}
